32bdba528fccbc61630e96adee47b6c0a2be5495
ChaonengTan <chaonengtan@gmail.com>
2021-04-26 15:38:44 -0700

    +lint

--- functions.js
+++ functions.js
@@ -9,20 +9,20 @@
 // }
 function getWeather(path) {
   return fetch(path)
-    .then(res => res.json())
-    .catch(err => console.log(err.message))
+    .then((res) => res.json())
+    .catch((err) => console.log(err.message));
 }
 function getWeatherZip(apiKey, zip, units = 'imperial') {
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-  getWeather(path)
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
+  getWeather(path);
 }
 function getWeatherGeo(apiKey, lat, lon, units = 'imperial') {
-  const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
-  getWeather(path)
+  const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  getWeather(path);
 }
 function getWeatherCity(apiKey, city, units = 'imperial') {
-  const path = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}&units=${units}`
-  getWeather(path)
+  const path = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}&units=${units}`;
+  getWeather(path);
 }
 // async function getWeather(apiKey, zip, units = 'imperial') {
 //   const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
@@ -35,36 +35,36 @@ function getWeatherCity(apiKey, city, units = 'imperial') {
 //     return err
 //   }
 // }
+function formatJson(json) {
+  return {
+    temp: json.main.temp,
+    humidity: json.main.humidity,
+    feelsLike: json.main.feels_like,
+    coord: json.coord,
+    country: json.sys.country,
+    city: json.name,
+    weather: json.weather[0].main,
+    description: json.weather[0].description,
+    windspeed: json.wind.speed,
+  };
+}
 function obtainWeather(path) {
   return fetch(path)
-    .then(res => res.json())
-    .then(json => formatJson(json))
-    .catch(err => console.log(err.message))
+    .then((res) => res.json())
+    .then((json) => formatJson(json))
+    .catch((err) => console.log(err.message));
 }
 function obtainWeatherZip(apiKey, zip, units = 'imperial') {
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-  obtainWeather(path)
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`;
+  obtainWeather(path);
 }
 function obtainWeatherGeo(apiKey, lat, lon, units = 'imperial') {
-  const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`
-  obtainWeather(path)
+  const path = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  obtainWeather(path);
 }
 function obtainWeatherCity(apiKey, city, units = 'imperial') {
-  const path = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}&units=${units}`
-  obtainWeather(path)
-}
-function formatJson(json){
-  return {
-    'temp': json.main.temp,
-    'humidity': json.main.humidity,
-    'feelsLike': json.main.feels_like,
-    'coord': json.coord,
-    'country': json.sys.country,
-    'city': json.name,
-    'weather': json.weather[0].main,
-    'description': json.weather[0].description,
-    'windspeed': json.wind.speed
-}
+  const path = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}&units=${units}`;
+  obtainWeather(path);
 }
 export {
   getWeatherZip,
@@ -73,4 +73,4 @@ export {
   obtainWeatherGeo,
   getWeatherCity,
   obtainWeatherCity,
-}
+};
